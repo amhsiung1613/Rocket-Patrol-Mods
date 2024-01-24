512840f1e60f9cb9e4031a74c52851bfdd008557
amhsiung <amhsiung@ucsc.edu>
2024-01-24 00:08:43 -0800

    typo

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -50,7 +50,7 @@ class Menu extends Phaser.Scene {
         this.add.text(game.config.width/2, game.config.height/2, 'Use <- -> arrows to move & (F) to fire', menuConfig).setOrigin(0.5)
         menuConfig.backgroundColor = '#00FF00'
         menuConfig.color = '#000'
-        this.add.text(game.config.width/2, game.config.height/2 +borderUISize + borderPadding, 'Press <- fro Novice or -> for Expert', menuConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/2 +borderUISize + borderPadding, 'Press <- for Novice or -> for Expert', menuConfig).setOrigin(0.5)
 
         //define keys
         keyLeft = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT)
